cf4e806919dc8e4c1e8f88b4d7e447f3b4b5acaa
utopictown <ekoprstyo.mail@gmail.com>
2022-02-12 17:51:36 +0700

    refactor: :recycle: refactor app js, display dynamic review item, submit review

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,44 +1,35 @@
+// selector
 const modal = document.getElementById("modal");
+const modalContainer = modal.getElementsByClassName("modal-container")[0];
+const reviewsContainer = document.getElementsByClassName("reviews-container")[0];
+const addButton = document.getElementById("add-review");
+const submitReview = document.getElementById("submit-review");
+const starReview = document.querySelectorAll(".input-star label");
+// end of selector
+
+// variables
+const MAX_STARS = 5;
+// end of variables
+
+// style modification
+modalContainer.style.bottom = "-100vh";
 modal.style.opacity = 0;
 modal.style.zIndex = -1;
-const modalContainer = modal.getElementsByClassName("modal-container")[0];
-modalContainer.style.bottom = "-100vh";
+// end of style modification
 
-modal.addEventListener("click", function (e) {
-  if (e.target.id && e.target.id === "modal") {
-    toggle(false);
-  }
-});
-
-const addButton = document.getElementById("add-review");
+// event listener
+modal.addEventListener("click", (e) => handleModalClick(e));
 addButton.addEventListener("click", () => toggle(true));
+submitReview.addEventListener("click", (e) => handleSubmitButtonClick(e));
 
-const submitReview = document.getElementById("submit-review");
-submitReview.addEventListener("click", () => toggle(false));
-
-// asign rating value to star
-const starReview = document.querySelectorAll(".input-star label");
 const starCount = starReview.length;
 let rating = 0;
-
 for (const star of starReview) {
-  star.addEventListener("click", function (e) {
-    const ratingValue = e.target.dataset.value;
-
-    resetStar();
-
-    if (rating != ratingValue) {
-      for (let i = ratingValue; i > 0; i--) {
-        starReview[starCount - i].classList.add("active");
-      }
-      rating = ratingValue;
-    } else {
-      rating = 0;
-    }
-  });
+  star.addEventListener("click", (e) => handleStarClick(e));
 }
+// end of event listener
 
-function toggle(isActive = true) {
+const toggle = (isActive = true) => {
   if (isActive === true) {
     modal.style.opacity = 1;
     modal.style.zIndex = 999;
@@ -50,11 +41,115 @@ function toggle(isActive = true) {
     modal.style.zIndex = -1;
     modalContainer.style.bottom = "-50vh";
   }
-}
+};
 
-function resetStar() {
+const resetStar = () => {
   for (let i = 0; i < starCount; i++) {
-    if (starReview[i].classList.contains("active"))
-      starReview[i].classList.remove("active");
+    if (starReview[i].classList.contains("active")) starReview[i].classList.remove("active");
   }
-}
+};
+
+const handleModalClick = (e) => {
+  if (e.target.id && e.target.id === "modal") {
+    toggle(false);
+  }
+};
+
+const handleStarClick = (e) => {
+  const ratingValue = e.target.dataset.value;
+
+  resetStar();
+
+  if (rating != ratingValue) {
+    for (let i = ratingValue; i > 0; i--) {
+      starReview[starCount - i].classList.add("active");
+    }
+    rating = ratingValue;
+  } else {
+    rating = 0;
+  }
+};
+
+const handleSubmitButtonClick = async (e) => {
+  e.preventDefault();
+  const rateSelector = document.querySelector("input[name=rate]:checked");
+  const rating = rateSelector ? rateSelector.value : null;
+  const description = document.getElementById("input-review").value;
+
+  if (rating && description) {
+    await fetchAPI("POST", "http://127.0.0.1:5000", {
+      rating,
+      description,
+    });
+    toggle(false);
+  }
+};
+
+const fetchAPI = async (method, url, payload = {}) => {
+  const resources = {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    method: method ?? "GET",
+    body: method != "GET" ? JSON.stringify(payload) : null,
+  };
+
+  const data = await fetch(url, resources);
+
+  let result = {
+    status: data.status,
+    ok: data.ok,
+  };
+
+  if (data.ok === true) {
+    result = { ...result, ...(await data.json()) };
+  } else {
+    result = [];
+  }
+
+  return result;
+};
+
+const init = async () => {
+  const data = await fetchAPI("GET", "http://127.0.0.1:5000");
+  data.data.forEach((review) => {
+    let stars = Array(Math.floor(review.rating))
+      .fill()
+      .map(
+        () =>
+          `<div class="star-wrapper">
+            <span class="star"></span>
+          </div>`
+      )
+      .join("");
+
+    if (Math.floor(review.rating) < MAX_STARS) {
+      stars += Array(MAX_STARS - Math.floor(review.rating))
+        .fill()
+        .map(
+          () =>
+            `<div class="star-wrapper">
+          <span class="empty-star"></span>
+        </div>`
+        )
+        .join("");
+    }
+
+    const reviewElement = `
+      <div class="review flex">
+        <div class="review-item flex">
+          <div class="stars">
+          ${stars}
+          </div>
+          <span>
+            <b>${review.rating}</b>, ${review.description}
+          </span>
+        </div>
+      </div>
+      `;
+    reviewsContainer.innerHTML += reviewElement;
+  });
+};
+
+init();
